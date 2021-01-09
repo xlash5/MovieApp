5557b836e7ff8f355b4fc411a03572c40a62dc25
Muhammet Enes Ilhan <58420282+xlash5@users.noreply.github.com>
2021-01-10 01:25:48 +0300

    Update Api.js

--- constants/Api.js
+++ constants/Api.js
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = '?api_key=28cfed2bc88bfd40c51aa916b0ab6357';
+export const API_KEY = '?api_key=API_KEY';
 export const IMAGE_PATH = 'https://image.tmdb.org/t/p/original/';
